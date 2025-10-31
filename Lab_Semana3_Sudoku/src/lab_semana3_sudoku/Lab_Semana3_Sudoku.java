4aa337c10b11af0f67ca3a59783afe0d51cb2164
DJCC <david.carcamo2006@gmail.com>
2025-10-31 08:17:13 -0600

    creacion de proyecto

---
+++ Lab_Semana3_Sudoku/src/lab_semana3_sudoku/Lab_Semana3_Sudoku.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_semana3_sudoku;
+
+/**
+ *
+ * @author David
+ */
+public class Lab_Semana3_Sudoku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
